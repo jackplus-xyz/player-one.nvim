1cabdfc4641da5a48936270fc94f17800297fd56
Jack Huang <jackplus.xyz@icloud.com>
2025-02-03 10:31:07 -0800

    refactor: improve binary loading

--- lua/player-one/lib.lua
+++ lua/player-one/lib.lua
@@ -1,14 +1,14 @@
 local M = {}
 
 local function get_lib_extension()
-	local system = jit.os
-	if system == "Windows" then
-		return "dll"
-	elseif system == "OSX" then
-		return "dylib"
-	else -- Linux and other Unix-like systems
-		return "so"
+	local os = jit.os:lower()
+	if os == "mac" or os == "osx" then
+		return ".dylib"
 	end
+	if os == "windows" then
+		return ".dll"
+	end
+	return ".so"
 end
 
 local function get_abs_path()
@@ -16,7 +16,7 @@ local function get_abs_path()
 	local source = debug_info.source
 	local plugin_root = source:sub(2):match("(.*)/lua/.*$") -- Remove '@' and get plugin root
 	local lib_ext = get_lib_extension()
-	return plugin_root .. "/target/debug/?." .. lib_ext
+	return plugin_root .. "/target/release/?" .. lib_ext
 end
 
 package.cpath = package.cpath .. ";" .. get_abs_path()
