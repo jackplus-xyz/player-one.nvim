81d9ae9cf074dd233c8627c8e08642c5d397ae4e
Jack Huang <jackplus.xyz@icloud.com>
2025-02-04 11:55:50 -0800

    refactor: improve lib loading

--- lua/player-one/lib.lua
+++ lua/player-one/lib.lua
@@ -1,5 +1,4 @@
-local M = {}
-
+-- @https://github.com/Saghen/blink.cmp/blob/85d0ca07184d783f6646d19c1af2c4ba970d98b2/lua/blink/cmp/fuzzy/rust.lua#L4)
 local function get_lib_extension()
 	local os = jit.os:lower()
 	if os == "mac" or os == "osx" then
@@ -11,16 +10,16 @@ local function get_lib_extension()
 	return ".so"
 end
 
-local function get_abs_path()
-	local debug_info = debug.getinfo(1, "S")
-	local source = debug_info.source
-	local plugin_root = source:sub(2):match("(.*)/lua/.*$") -- Remove '@' and get plugin root
-	local lib_ext = get_lib_extension()
-	return plugin_root .. "/target/release/?" .. lib_ext
-end
+-- search for the lib in the /target/release directory with and without the lib prefix
+-- since MSVC doesn't include the prefix
+package.cpath = package.cpath
+	.. ";"
+	.. debug.getinfo(1).source:match("@?(.*/)")
+	.. "../../target/release/lib?"
+	.. get_lib_extension()
+	.. ";"
+	.. debug.getinfo(1).source:match("@?(.*/)")
+	.. "../../target/release/?"
+	.. get_lib_extension()
 
-package.cpath = package.cpath .. ";" .. get_abs_path()
-
-M = require("libplayerone")
-
-return M
+return require("libplayerone")
