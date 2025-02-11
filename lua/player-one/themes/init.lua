c7f4ba0ce28b27473e8cb7ce141e0c9109ffd865
Jack Huang <jackplus.xyz@icloud.com>
2025-02-10 16:20:21 -0800

    refactor: move theme functions to utils module

--- lua/player-one/themes/init.lua
+++ lua/player-one/themes/init.lua
@@ -3,61 +3,6 @@ local Utils = require("player-one.utils")
 
 local M = {}
 
-local group = vim.api.nvim_create_augroup("PlayerOne", { clear = true })
-
-local function create_autocmds(autocmd, sound, callback)
-	vim.api.nvim_create_autocmd(autocmd, {
-		group = group,
-		callback = function()
-			if State.is_enabled then
-				if callback then
-					callback(sound)
-				else
-					Utils.play(sound)
-				end
-			end
-		end,
-	})
-end
-
-local function clear_autocmds()
-	vim.api.nvim_clear_autocmds({ group = group })
-end
-
-function M.load(theme)
-	if theme == "default" or not theme then
-		theme = State.curr_theme
-		if not theme then
-			return
-		end
-	end
-
-	clear_autocmds()
-
-	-- TODO: add more preset?
-	-- { "chiptune" ,"synth" ,"crystal" ,"mechanical" ,"minimal" ,"retro" ,"ambient" ,"digital" }
-	local presets = { "chiptune", "synth", "crystal" }
-	if type(theme) == "string" then
-		if not vim.tbl_contains(presets, theme) then
-			error(string.format("Invalid preset '%s'. Available presets: %s", theme, table.concat(presets, ", ")))
-		end
-		theme = require("player-one.themes." .. theme)
-	end
-
-	for i, v in ipairs(theme) do
-		if type(v) ~= "table" then
-			error(string.format("Invalid sound configuration at index %d", i))
-		end
-		if not v.event then
-			error(string.format("Missing 'event' in sound configuration at index %d", i))
-		end
-		if not v.sound then
-			error(string.format("Missing 'sound' in sound configuration at index %d", i))
-		end
-		create_autocmds(v.event, v.sound, v.callback)
-	end
-end
-
 function M.setup()
 	local theme = State.theme
 
@@ -70,7 +15,7 @@ function M.setup()
 	end
 
 	State.curr_theme = theme
-	M.load(theme)
+	Utils.load_theme(theme)
 end
 
 return M
--- lua/player-one/utils.lua
+++ lua/player-one/utils.lua
@@ -4,6 +4,7 @@ local State = require("player-one.state")
 local M = {}
 
 local last_play_time = 0
+local group = vim.api.nvim_create_augroup("PlayerOne", { clear = true })
 
 local function sanitize_params(params)
 	if not params then
@@ -152,6 +153,60 @@ local function process_sound_params(params, callback)
 	end
 
 	error(string.format("Invalid sound params type: %s", type(params)))
+end
+
+function M.create_autocmds(autocmd, sound, callback)
+	vim.api.nvim_create_autocmd(autocmd, {
+		group = group,
+		callback = function()
+			if State.is_enabled then
+				if callback then
+					callback(sound)
+				else
+					M.play(sound)
+				end
+			end
+		end,
+	})
+end
+
+function M.clear_autocmds()
+	-- FIXME: not really clearing all autocmds
+	vim.api.nvim_clear_autocmds({ group = group })
+end
+
+function M.load_theme(theme)
+	if theme == "default" or not theme then
+		theme = State.curr_theme
+		if not theme then
+			return
+		end
+	end
+
+	M.clear_autocmds()
+
+	-- TODO: add more preset?
+	-- { "chiptune" ,"synth" ,"crystal" ,"mechanical" ,"minimal" ,"retro" ,"ambient" ,"digital" }
+	local presets = { "chiptune", "synth", "crystal" }
+	if type(theme) == "string" then
+		if not vim.tbl_contains(presets, theme) then
+			error(string.format("Invalid preset '%s'. Available presets: %s", theme, table.concat(presets, ", ")))
+		end
+		theme = require("player-one.themes." .. theme)
+	end
+
+	for i, v in ipairs(theme) do
+		if type(v) ~= "table" then
+			error(string.format("Invalid sound configuration at index %d", i))
+		end
+		if not v.event then
+			error(string.format("Missing 'event' in sound configuration at index %d", i))
+		end
+		if not v.sound then
+			error(string.format("Missing 'sound' in sound configuration at index %d", i))
+		end
+		M.create_autocmds(v.event, v.sound, v.callback)
+	end
 end
 
 function M.play(params)
