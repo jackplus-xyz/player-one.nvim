0e735d86d519e9d6149aeacce49b6b0eae60c5c2
Jack Huang <jackplus.xyz@icloud.com>
2025-02-12 21:25:38 -0800

    feat(theme): update synth theme with simplified sounds

--- lua/player-one/themes/synth.lua
+++ lua/player-one/themes/synth.lua
@@ -1,127 +1,102 @@
 local Utils = require("player-one.utils")
-local group = vim.api.nvim_create_augroup("PlayerOne", { clear = true })
+
+local is_cursormoved_enabled = false
 
 return {
 	{
 		event = "VimEnter",
 		sound = {
-			-- A deep, rich synth chord. Lower frequencies help make things sound more bassy.
 			{
-				wave_type = 2,
-				base_freq = 65.41, -- C2
-				env_attack = 0.05,
-				env_sustain = 0.15,
-				env_decay = 0.2,
-				duty = 43.55,
-				duty_ramp = 0,
-				lpf_freq = 29910,
-				lpf_ramp = 4.645,
-				lpf_resonance = 92.65,
-				hpf_freq = 0,
-				hpf_ramp = 3.300,
+				wave_type = 1,
+				base_freq = 277.18,
+				env_attack = 0.0,
+				env_sustain = 0.001,
+				env_decay = 0.15,
+				duty = 0.6,
+				lpf_freq = 4000,
 			},
 			{
-				wave_type = 2,
-				base_freq = 82.41, -- E2
-				env_attack = 0.05,
-				env_sustain = 0.15,
-				env_decay = 0.2,
-				duty = 43.55,
-				duty_ramp = 0,
-				lpf_freq = 29910,
-				lpf_ramp = 4.645,
-				lpf_resonance = 92.65,
-				hpf_freq = 0,
-				hpf_ramp = 3.300,
+				wave_type = 1,
+				base_freq = 369.99,
+				env_attack = 0.0,
+				env_sustain = 0.001,
+				env_decay = 0.15,
+				duty = 0.6,
+				lpf_freq = 4000,
 			},
 			{
-				wave_type = 2,
-				base_freq = 98.00, -- G2
-				env_attack = 0.05,
-				env_sustain = 0.15,
-				env_decay = 0.2,
-				duty = 43.55,
-				duty_ramp = 0,
-				lpf_freq = 29910,
-				lpf_ramp = 4.645,
-				lpf_resonance = 92.65,
-				hpf_freq = 0,
-				hpf_ramp = 3.300,
+				wave_type = 1,
+				base_freq = 440.00,
+				env_attack = 0.0,
+				env_sustain = 0.001,
+				env_decay = 0.15,
+				duty = 0.6,
+				lpf_freq = 4000,
 			},
 			{
-				wave_type = 2,
-				base_freq = 130.81, -- C3
-				env_attack = 0.05,
-				env_sustain = 0.15,
-				env_decay = 0.3,
-				duty = 43.55,
-				duty_ramp = 0,
-				lpf_freq = 29910,
-				lpf_ramp = 4.645,
-				lpf_resonance = 92.65,
-				hpf_freq = 0,
-				hpf_ramp = 3.300,
+				wave_type = 1,
+				base_freq = 554.37,
+				env_attack = 0.0,
+				env_sustain = 0.001,
+				env_decay = 0.15,
+				duty = 0.6,
+				lpf_freq = 4000,
+			},
+			{
+				wave_type = 1,
+				base_freq = 739.99,
+				env_attack = 0.0,
+				env_sustain = 0.001,
+				env_decay = 0.15,
+				duty = 0.6,
+				lpf_freq = 4000,
 			},
 		},
 		callback = function(sound)
-			Utils.play(sound)
-
-			-- Delay registering the CursorMoved autocmd until 1 second after VimEnter.
+			Utils.append(sound)
 			vim.defer_fn(function()
-				vim.api.nvim_create_autocmd("CursorMoved", {
-					group = group,
-					callback = function()
-						local cursor_sound = {
-							wave_type = 2,
-							base_freq = 55.00, -- A1 for a deep pulse on movement
-							env_attack = 0.01,
-							env_sustain = 0.1,
-							env_decay = 0.05,
-							duty = 43.55,
-							duty_ramp = 0,
-							lpf_freq = 29910,
-							lpf_ramp = 4.645,
-							lpf_resonance = 92.65,
-							hpf_freq = 0,
-							hpf_ramp = 3.300,
-						}
-						Utils.play(cursor_sound)
-					end,
-				})
+				is_cursormoved_enabled = true
 			end, 1000)
+		end,
+	},
+	{
+		event = "CursorMoved",
+		sound = {
+			wave_type = 1,
+			base_freq = 277.18,
+			env_attack = 0.001,
+			env_sustain = 0.01,
+			env_decay = 0.08,
+			duty = 0.3,
+			lpf_freq = 2000,
+		},
+		callback = function(sound)
+			if not is_cursormoved_enabled then
+				return
+			end
+			Utils.play(sound)
 		end,
 	},
 	{
 		event = "VimLeavePre",
 		sound = {
-			-- An electric farewell in the upper bass range.
 			{
-				wave_type = 2,
-				base_freq = 98.00, -- G2
+				wave_type = 1,
+				base_freq = 220.00,
 				env_attack = 0.0,
-				env_sustain = 0.05,
+				env_sustain = 0.001,
 				env_decay = 0.15,
-				duty = 43.55,
-				duty_ramp = 0,
-				lpf_freq = 29910,
-				lpf_ramp = 4.645,
-				lpf_resonance = 92.65,
-				hpf_freq = 0,
-				hpf_ramp = 3.300,
+				duty = 0.6,
+				lpf_freq = 4000,
 			},
 			{
-				wave_type = 2,
-				base_freq = 123.47, -- B2
+				wave_type = 1,
+				base_freq = 293.66,
 				env_attack = 0.0,
-				env_sustain = 0.05,
+				env_sustain = 0.001,
 				env_decay = 0.15,
-				duty = 43.55,
-				duty_ramp = 0,
-				lpf_freq = 29910,
-				lpf_ramp = 4.645,
-				lpf_resonance = 92.65,
-				hpf_freq = 0,
-				hpf_ramp = 3.300,
+				duty = 0.6,
+				lpf_freq = 4000,
 			},
 		},
 		callback = function(sound)
@@ -131,139 +106,99 @@ return {
 	{
 		event = "BufWritePost",
 		sound = {
-			-- A short, bass-punch combo triggered on file save.
 			{
-				wave_type = 2,
-				base_freq = 82.41, -- E2
-				env_attack = 0.02,
-				env_sustain = 0.1,
+				wave_type = 1,
+				base_freq = 880.0,
+				env_attack = 0.0,
+				env_sustain = 0.001,
 				env_decay = 0.15,
-				duty = 43.55,
-				duty_ramp = 0,
-				lpf_freq = 29910,
-				lpf_ramp = 4.645,
-				lpf_resonance = 92.65,
-				hpf_freq = 0,
-				hpf_ramp = 3.300,
+				duty = 0.3,
+				lpf_freq = 3000,
 			},
 			{
-				wave_type = 2,
-				base_freq = 73.42, -- D2
-				env_attack = 0.02,
-				env_sustain = 0.1,
+				wave_type = 1,
+				base_freq = 987.77,
+				env_attack = 0.0,
+				env_sustain = 0.001,
 				env_decay = 0.15,
-				duty = 43.55,
-				duty_ramp = 0,
-				lpf_freq = 29910,
-				lpf_ramp = 4.645,
-				lpf_resonance = 92.65,
-				hpf_freq = 0,
-				hpf_ramp = 3.300,
+				duty = 0.3,
+				lpf_freq = 3000,
 			},
 		},
+		callback = "append",
 	},
 	{
 		event = "TextChangedI",
 		sound = {
-			-- A quick electric pulse as you change text.
-			wave_type = 2,
-			base_freq = 87.31, -- F2
-			env_attack = 0.02,
-			env_sustain = 0.07,
-			env_decay = 0.05,
-			duty = 43.55,
-			duty_ramp = 0,
-			lpf_freq = 29910,
-			lpf_ramp = 4.645,
-			lpf_resonance = 92.65,
-			hpf_freq = 0,
-			hpf_ramp = 3.300,
+			wave_type = 1,
+			base_freq = 415.30,
+			env_attack = 0.001,
+			env_sustain = 0.01,
+			env_decay = 0.08,
+			duty = 0.3,
+			lpf_freq = 2000,
 		},
+		callback = "play",
 	},
 	{
 		event = "TextYankPost",
 		sound = {
-			-- Two-note confirmation with an electric edge.
 			{
-				wave_type = 2,
-				base_freq = 110.00, -- A2
+				wave_type = 1,
+				base_freq = 587.33,
 				env_attack = 0.0,
-				env_sustain = 0.08,
+				env_sustain = 0.001,
 				env_decay = 0.15,
-				duty = 43.55,
-				duty_ramp = 0,
-				lpf_freq = 29910,
-				lpf_ramp = 4.645,
-				lpf_resonance = 92.65,
-				hpf_freq = 0,
-				hpf_ramp = 3.300,
+				duty = 0.6,
+				lpf_freq = 4000,
 			},
 			{
-				wave_type = 2,
-				base_freq = 123.47, -- B2
+				wave_type = 1,
+				base_freq = 659.25,
 				env_attack = 0.0,
-				env_sustain = 0.08,
+				env_sustain = 0.001,
 				env_decay = 0.15,
-				duty = 43.55,
-				duty_ramp = 0,
-				lpf_freq = 29910,
-				lpf_ramp = 4.645,
-				lpf_resonance = 92.65,
-				hpf_freq = 0,
-				hpf_ramp = 3.300,
+				duty = 0.6,
+				lpf_freq = 4000,
 			},
 		},
+		callback = "append",
 	},
 	{
 		event = "CmdlineEnter",
 		sound = {
-			-- A set of dual tones to signal command-line engagement.
 			{
-				wave_type = 2,
-				base_freq = 73.42, -- D2
-				env_attack = 0.03,
-				env_sustain = 0.1,
-				env_decay = 0.05,
-				duty = 43.55,
-				duty_ramp = 0,
-				lpf_freq = 29910,
-				lpf_ramp = 4.645,
-				lpf_resonance = 92.65,
-				hpf_freq = 0,
-				hpf_ramp = 3.300,
+				wave_type = 1,
+				base_freq = 329.63,
+				env_attack = 0.0,
+				env_sustain = 0.001,
+				env_decay = 0.15,
+				duty = 0.6,
+				lpf_freq = 4000,
 			},
 			{
-				wave_type = 2,
-				base_freq = 82.41, -- E2
-				env_attack = 0.03,
-				env_sustain = 0.1,
-				env_decay = 0.05,
-				duty = 43.55,
-				duty_ramp = 0,
-				lpf_freq = 29910,
-				lpf_ramp = 4.645,
-				lpf_resonance = 92.65,
-				hpf_freq = 0,
-				hpf_ramp = 3.300,
+				wave_type = 1,
+				base_freq = 392.00,
+				env_attack = 0.0,
+				env_sustain = 0.001,
+				env_decay = 0.15,
+				duty = 0.6,
+				lpf_freq = 4000,
 			},
 		},
+		callback = "append",
 	},
 	{
 		event = "CmdlineChanged",
 		sound = {
-			-- An electric pulse on command change.
-			wave_type = 2,
-			base_freq = 98.00, -- G2
-			env_attack = 0.03,
-			env_sustain = 0.1,
-			env_decay = 0.05,
-			duty = 43.55,
-			duty_ramp = 0,
-			lpf_freq = 29910,
-			lpf_ramp = 4.645,
-			lpf_resonance = 92.65,
-			hpf_freq = 0,
-			hpf_ramp = 3.300,
+			wave_type = 1,
+			base_freq = 493.88,
+			env_attack = 0.001,
+			env_sustain = 0.01,
+			env_decay = 0.08,
+			duty = 0.3,
+			lpf_freq = 2000,
 		},
+		callback = "play",
 	},
 }
